2fefdcacbc0b29cee0c8c0e9525cac1d26bff73a
sk32 <a01208733@itesm.mx>
2018-11-26 14:24:37 -0600

    Terminar modificar categoria servicio

--- Proyecto/php/_consultar_catservicio.php
+++ Proyecto/php/_consultar_catservicio.php
@@ -1,11 +1,25 @@
 <?php
     $menu = "ajustes";
     session_start();
+    require_once("./model_modificar_catservicio.php");
     if (isset($_SESSION["Usuario"])){
-        include('../html/_header.html');
-        include('../html/_menu.html');
-        include('../html/Ajustes/Servicios/_consultar_catservicio.html');
-        include('../html/_footer.html');
+        if($_SESSION["Permisos"][6] == TRUE){
+            include('../html/_header.html');
+            include('../html/_menu.html');
+            if(isset($_POST['nombreCatSerBus'])){
+                $nombreCatSerBus = $_POST['nombreCatSerBus'];
+                $tabla = buscar_cat_ser($nombreCatSerBus);
+                include('../html/Ajustes/Servicios/_consultar_catservicio.html');
+            }else{
+                include('../html/Ajustes/Servicios/_consultar_catservicio.html');
+            }
+            include('../html/_footer.html');
+        }else{
+            session_unset();
+            session_destroy();
+            header("location:/index.php");
+        }
+        
     } else {
         header("location:/index.php");
     }
